1d073fbf5388eb563b1249dd2b8d3817252dd2ad
Dudu Matei <mateiioan.dudu@gmail.com>
2021-11-29 22:12:37 +0200

    Started writing the code

--- Task1.c
+++ Task1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+#define MAX 100
+
+void afisare(int matrix[MAX][MAX],int n,int m)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    
+    char cod[MAX];
+    int matrix[MAX][MAX]={0};
+    int n,m;
+    scanf("%d%d",&n,&m);
+    afisare(matrix,n,m);
+    fgets(cod,MAX,stdin);
+    printf("%s",cod);
     return 0;
 }
