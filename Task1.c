c68380186b392008592a97364e7b991fa4f0a5e5
Dudu Matei <mateiioan.dudu@gmail.com>
2021-11-30 10:42:24 +0200

    Added case for strings starting with 'a'

--- Task1.c
+++ Task1.c
@@ -18,10 +18,63 @@ int main()
 {
     char cod[MAX];
     int matrix[MAX][MAX]={0};
-    int n,m;
+    int n,m,x=0,y=0,pas=0;
+    matrix[x][x]=++pas;
     scanf("%d%d",&n,&m);
-    afisare(matrix,n,m);
-    fgets(cod,MAX,stdin);
-    printf("%s",cod);
+    while(scanf("%s",cod))
+    {
+        switch(cod[0])
+        {
+            case 'a':
+            {
+                int max=0;
+                int pozmax=1;
+                for(int i=1;i<=4;i++)
+                {
+                    if(cod[i]-'0'>max)
+                    {
+                        max=cod[i]-'0';
+                        pozmax=i;
+                    }
+                }
+                printf("%d %d\n",max,pozmax);
+                switch(pozmax)
+                {
+                    case 1:
+                    {
+                        y++;
+                        matrix[x][y]=++pas;
+                        break;
+                    }
+                    case 2:
+                    {
+                        x--;
+                        matrix[x][y]=++pas;
+                        break;
+                    }
+                    case 3:
+                    {
+                        y--;
+                        matrix[x][y]=++pas;
+                        break;
+                    }
+                    case 4:
+                    {
+                        x++;
+                        matrix[x][y]=++pas;
+                        break;
+                    }
+                    default:
+                        printf("CEVA");
+                }
+                afisare(matrix,n,m);
+                break;
+            }
+            case 'b':
+            {
+                
+            }
+        }
+    }
     return 0;
 }
