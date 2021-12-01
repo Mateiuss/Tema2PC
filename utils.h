e3e42499364ad6db953fbdd08c89cb2fc501b71e
Matei Dudu <mateiioan.dudu@gmail.com>
2021-12-01 16:26:20 +0200

    Added utils.h

---
+++ utils.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define MAX 100
+void afisare(int matrix[MAX][MAX],int,int); 
+int prim(int);
+void SolveTask1();
+void SolveTask2();
+void SolveTask3();
