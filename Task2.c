ec3fcbb36002507398e1cc6d8119fff3a17e3a8a
Matei Dudu <mateiioan.dudu@gmail.com>
2021-12-02 14:02:53 +0200

    Added some more functions

--- Task2.c
+++ Task2.c
@@ -8,29 +8,82 @@ void SolveTask2() {
 	case 'c':
 	{
 		int key;
-		char cod[MAX];
+		char cod[MAX2];
 		scanf("%d\n",&key);
 		fgets(cod,MAX,stdin);
-		for(int i=0;i<strlen(cod);i++){
-			if(cod[i]>='0'&&cod[i]<='9'){
-				cod[i]-=key%10;
-				if(cod[i]<'0')
-					cod[i]='9'-('0'-cod[i])+1;
-			}
-			else if(cod[i]>='a'&&cod[i]<='z'){
-				cod[i]-=key%26;
-				if(cod[i]<'a')
-                                        cod[i]='z'-('a'-cod[i])+1;
-			}
-			else if(cod[i]>='A'&&cod[i]<='Z'){
-				cod[i]-=key%26;
-                                if(cod[i]<'A')
-                                        cod[i]='Z'-('A'-cod[i])+1;
-                        }
-		}
+		decriptareCaesar(cod,key);
 		printf("%s",cod);
 		break;
 	}
+	case 'v':
+	{
+		char cod[MAX2],key[11];
+		fgets(key,11,stdin);
+		fgets(cod,MAX2,stdin);
+		decriptareVigenere(cod,key);
+		printf("%s",cod);
+		break;	
+	}
+	case 'a':
+	{
+		int key;
+		char N1[MAX2],N2[MAX2],sum[MAX2];
+		scanf("%d\n",&key);
+		fgets(N1,MAX2,stdin);
+		fgets(N2,MAX2,stdin);
+		decriptareCaesar(N1,key);
+		decriptareCaesar(N2,key);
+		printf("N1=%s\nN2=%s\n",N1,N2);
+		sumChars(N1,N2,sum);
+	}
 	default: printf("Hi");
     }
+}
+
+void decriptareCaesar(char *cod,int key){
+	for(int i=0;i<strlen(cod);i++){
+		if(cod[i]>='0'&&cod[i]<='9'){
+			cod[i]-=key%10;
+			if(cod[i]<'0')
+				cod[i]='9'-('0'-cod[i])+1;
+		}
+		else if(cod[i]>='a'&&cod[i]<='z'){
+			cod[i]-=key%26;
+			if(cod[i]<'a')
+				cod[i]='z'-('a'-cod[i])+1;
+		}
+		else if(cod[i]>='A'&&cod[i]<='Z'){
+			cod[i]-=key%26;
+			if(cod[i]<'A')
+				cod[i]='Z'-('A'-cod[i])+1;
+		}
+	}
+}
+
+void decriptareVigenere(char *cod,char *key){
+	for(int i=0;i<strlen(cod);i++){
+		int key2=key[i%(strlen(key)-1)]-'A';
+		if(cod[i]>='0'&&cod[i]<='9'){
+			cod[i]-=key2%10;
+			if(cod[i]<'0')
+				cod[i]='9'-('0'-cod[i])+1;
+		}
+		else if(cod[i]>='a'&&cod[i]<='z'){
+			cod[i]-=key2%26;
+			if(cod[i]<'a')
+				cod[i]='z'-('a'-cod[i])+1;
+		}
+		else if(cod[i]>='A'&&cod[i]<='Z'){
+			cod[i]-=key2%26;
+			if(cod[i]<'A')
+				cod[i]='Z'-('A'-cod[i])+1;
+		}
+	}
+}
+
+void sumChars(char *N1,char *N2,char *sum){
+	
+	if(strlen(N1) < strlen(N2)){
+		for(int i
+	}
 }
--- utils.h
+++ utils.h
@@ -4,9 +4,13 @@
 #include <math.h>
 #include <string.h>
 #define MAX 200
+#define MAX2 1000
 #define CIFLEN 20
 void afisare(int matrix[MAX][MAX],int,int); 
 int prim(int);
+void decriptareCaesar(char*,int);
+void decriptareVigenere(char*,char*);
+void sumChars(char*,char*,char*);
 void SolveTask1();
 void SolveTask2();
 void SolveTask3();
