9f0b54d41ad6a25b06822141bd49dd86b3e83e0f
Matei Dudu <mateiioan.dudu@gmail.com>
2021-12-08 01:27:38 +0200

    Finished Task3.c

--- Task3.c
+++ Task3.c
@@ -1,125 +1,59 @@
 #include "utils.h"
 
 void SolveTask3() {
-    char sir[MAX3],sirPrelucrat[MAX3];
-    citire(sir);
-    strcpy(sirPrelucrat,sir);
-    prelucrareSir(sirPrelucrat);
-    printf("%s\n",sirPrelucrat);
-    printf("%d\n",nr2grame(sirPrelucrat,sir));
-    nr2grameComb(sirPrelucrat);
+	char sir[MAX3], doigrame[LIN][COL];
+	int aparitii[MAX2] = { 0 }, nrAparitii = 0;
+	citire(sir);
+	salvare_2grame(doigrame, sir, aparitii, &nrAparitii);
+	printf("%d\n", nrAparitii);
+	for (int i = 1; i <= nrAparitii; i++) {
+		printf("%s %d\n", doigrame[i], aparitii[i]);
+	}
 }
 
-void nr2grameComb(char *sir){
-	char *p,*b,csir[MAX3];
-	strcpy(csir,sir);
-	b=strtok(csir," ,.!;");
-	p=strtok(NULL," ,.!;");
-	while(p){
-		char str[MAX3];
-		strcpy(str,b);
-		strcat(str," ");
-		strcat(str,p);
-		addNull(str);
-		int aparitii=nrAparitii(p+strlen(p)+1,str);
-		printf("%s %d\n",str,aparitii+1);
-		if(aparitii){
-			stergeSir(p+strlen(p)+1,str);
-			//printf("p+strlen(p)+1:%s\n",p+strlen(p)+1);
+int cautare(char doigrame[LIN][COL], char *str, int *aparitii, int *nrAparitii) {
+	for (int i = 1; i <= *nrAparitii; i++) {
+		if(strcmp(doigrame[i], str) == 0) {
+			aparitii[i]++;
+			return 1;
 		}
-		b=strtok(NULL," ,.!;");
-		p=strtok(NULL," ,.!;");
 	}
+	return 0;
 }
 
-int nr2grame(char *sir,char *sirNrCuvinte){
-	int nr=nrCuvinte(sirNrCuvinte)-1;
-	char *p,*b,csir[MAX3];
-	strcpy(csir,sir);
-	b=strtok(csir," ,.!;");
-	p=strtok(NULL," ,.!;");
-	while(p){
-		char str[MAX3];
-		strcpy(str,b);
-		strcat(str," ");
-		strcat(str,p);
+void salvare_2grame(char doigrame[LIN][COL], char *sir, int *aparitii, int *nrAparitii) {
+	char *p, *q;
+	p = strtok(sir, " .,!;");
+	q = strtok(NULL, " .,!;");
+	addNull(p);
+	addNull(q);
+	while (q) {
+		char str[MAX2];
+		strcpy(str, p);
+		strcat(str, " ");
+		strcat(str, q);
 		addNull(str);
-		int aparitii=nrAparitii(p+strlen(p)+1,str);
-		if(aparitii>0){
-			nr=nr-aparitii;
-			stergeSir(p+strlen(p)+1,str);
+		//printf("%s\n", str);
+		if (cautare(doigrame, str, aparitii, nrAparitii) == 0) {
+			*nrAparitii = *nrAparitii + 1;
+			strcpy(doigrame[*nrAparitii], str);
+			aparitii[*nrAparitii]++;
 		}
-		b=strtok(NULL," ,.!;");
-		p=strtok(NULL," ,.!;");
-	}
-	return nr;
-}
-
-void stergeSir(char *sir,char *delSir){
-	char *a;
-	a=strstr(sir,delSir);
-	while(a){
-		char t[MAX3];
-		strcpy(t,a+strlen(delSir)+1);
-		strcpy(a,t);
-		addNull(a);
-		a=strstr(sir,delSir);
+		//printf("%d %d\n", *nrAparitii, aparitii[*nrAparitii]);
+		p = q;
+		q = strtok(NULL, " .,!;");
 	}
 }
 
-int nrAparitii(char *sir,char *p){
-	int nr=0;
-	char *a;
-	a=strstr(sir,p);
-	while(a){
-		if(*(a+strlen(p))==' '||*(a+strlen(p))=='\0')
-			nr++;
-		a=strstr(a+strlen(p)+1,p);
-	}
-	return nr;
-}
-
-int nrCuvinte(char *sir){
-	int nr=0;
-	char *p,csir[MAX3];
-	strcpy(csir,sir);
-	p=strtok(csir," ,.!;");
-	while(p){
-		nr++;
-		p=strtok(NULL," ,.!;");
-	}
-	return nr;
-}
-
-void prelucrareSir(char *sir){
-	char csir[MAX3],*p,*q;
-	strcpy(csir,sir);
-	sir[0]='\0';
-	q=strtok(csir," ,.!;");
-	p=strtok(NULL," ,.!;");
-	while(p){
-		char str[MAX3];
-		strcpy(str,q);
-		strcat(str," ");
-		strcat(str,p);
-		addNull(str);
-		strcat(sir,str);
-		strcat(sir," ");
-		q=p;
-		p=strtok(NULL," ,.!;");
-	}
-	addNull(sir);
-}
-
-void citire(char * sir){
-	char s[MAX3];
+void citire(char *sir) {
+	char str[MAX3];
 	getchar();
-	fgets(s,MAX3,stdin);
-	addNull(s);
-	strcpy(sir,s);
-	while(fgets(s,MAX3,stdin)){
-		addNull(s);
-		strcat(sir," ");
-		strcat(sir,s);
+	fgets(str, MAX3, stdin);
+	addNull(str);
+	strcpy(sir,str);
+	while (fgets(str, MAX3, stdin)){
+		addNull(str);
+		strcat(sir, " ");
+		strcat(sir, str);
 	}
 }
--- utils.h
+++ utils.h
@@ -7,6 +7,8 @@
 #define MAX2 1000
 #define MAX3 10000
 #define CIFLEN 20
+#define LIN 1000
+#define COL 50
 void afisare(int matrix[MAX][MAX],int,int);
 void citire(char*);
 int prim(int);
@@ -15,13 +17,8 @@ void decriptareCaesar(char*,int);
 void decriptareVigenere(char*,char*);
 void sumChars(char*,char*,char*);
 void addNull(char *);
-void addNUll2(char *);
-int nrAparitii(char*,char*);
-int nr2grame(char *,char*);
-void nr2grameComb(char *);
-int nrCuvinte(char*);
-void prelucrareSir(char*);
-void stergeSir(char*,char*);
+int cautare(char m[LIN][COL], char *, int *, int *);
+void salvare_2grame(char m[LIN][COL], char *, int *, int *);
 void SolveTask1();
 void SolveTask2();
 void SolveTask3();
